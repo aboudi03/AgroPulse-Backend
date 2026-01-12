10d2d508a8ed4598ed424080c443cd1c967d3da8
aboudi03 <aboudis03@hotmail.com>
2026-01-12 20:08:10 +0200

    f

--- src/main/java/com/agropulse/infrastructure/persistence/DatabaseSequence.java
+++ src/main/java/com/agropulse/infrastructure/persistence/DatabaseSequence.java
@@ -1,5 +1,6 @@
 package com.agropulse.infrastructure.persistence;
 
+import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 /**
@@ -9,6 +10,7 @@ import org.springframework.data.mongodb.core.mapping.Document;
 @Document(collection = "database_sequences")
 public class DatabaseSequence {
 
+    @Id
     private String id;
     private Long seq;
 
